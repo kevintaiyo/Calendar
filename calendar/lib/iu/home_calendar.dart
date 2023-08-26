5038615b30e4e7372e69c550d5bf0270d4cf1452
unknown <kevintaiyo17@gmail.com>
2023-08-26 12:45:13 -0300

    video1 finalizado

--- calendar/lib/iu/home_calendar.dart
+++ calendar/lib/iu/home_calendar.dart
@@ -1,5 +1,9 @@
+import 'dart:html';
+
+import 'package:calendar/servicos/servicos_notificacao.dart';
 import 'package:flutter/material.dart';
 import 'package:calendar/servicos/servicosTemas.dart';
+import 'package:get/get.dart';
 
 class HomeCalendar extends StatefulWidget {
   const HomeCalendar({super.key});
@@ -9,30 +13,49 @@ class HomeCalendar extends StatefulWidget {
 }
 
 class _HomeCalendarState extends State<HomeCalendar> {
+  var notificacao;
+
+  @override
+  void initState() {
+    super.initState();
+    notificacao = Notificacoes();
+    notificacao.requestIOSPermissions();
+    notificacao.initializeNotification();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _AppBar(),
-      body:   Column(
-        children: [
-          Text("Theme data", style: TextStyle(fontSize: 30),)
-        ]
-      ),
+      body: Column(children: [
+        Text(
+          "Theme data",
+          style: TextStyle(fontSize: 30),
+        )
+      ]),
     );
   }
 
-  _AppBar(){
+  _AppBar() {
     return AppBar(
       leading: GestureDetector(
         onTap: () {
           temasServicos().trocarTema();
+          notificacao.displayNotification(
+              Title: "TEMA TROCADO",
+              body:
+                  Get.isDarkMode ? "ATIVADO MODO DARK" : "ATIVADO MODO CLARO");
         },
         child: Icon(Icons.nightlight_round, size: 20),
-        
       ),
       actions: [
-        Icon(Icons.person, size: 20,),
-        SizedBox(width: 20,)
+        Icon(
+          Icons.person,
+          size: 20,
+        ),
+        SizedBox(
+          width: 20,
+        )
       ],
     );
   }
